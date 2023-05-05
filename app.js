2300c6652d8c4824e9702bc57b3d712ab8c5827d
av124773 <av124773@gmail.com>
2023-05-05 10:55:26 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose =  require('mongoose')
+const exphbs = require('express-handlebars')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -9,19 +10,19 @@ const app = express()
 const PORT = 3001
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
 const db = mongoose.connection
-
 db.on('error', () => {
   console.log('mongoose error!')
 })
-
 db.once('open', () => {
   console.log('mongoose connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('test route')
+  res.render('index')
 })
 
 app.listen(PORT, () => {
