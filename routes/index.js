675770db63c187dc881b81fc24c91232145e1777
av124773 <av124773@gmail.com>
2023-05-07 14:45:38 +0800

    feat: add users routes

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const home = require('./modules/home')
+const users = require('./modules/users')
 const record = require('./modules/record')
 
 
 router.use('/', home)
 router.use('/record', record)
+router.use('/users', users)
 
 module.exports = router
